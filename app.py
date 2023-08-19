dae94cee628b070f05b3b29de7ebcd926787651f
Himanshu <waitasecant@gmail.com>
2023-08-19 19:30:02 +0530

    Removed flask_cors

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request
-from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
-cors = CORS(app)
 model = pickle.load(open('RFModel.pkl', 'rb'))
-car = pd.read_csv('Concrete_Data.csv')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,7 +13,6 @@ def index():
 
 
 @app.route('/predict', methods=['POST'])
-@cross_origin()
 def predict():
     Cement = request.form.get('Cement')
     Blast_Furnace_Slag = request.form.get('Blast_Furnace_Slag')
@@ -25,7 +21,7 @@ def predict():
     Age = request.form.get('Age')
     prediction = model.predict(pd.DataFrame(columns=['Cement', 'Blast_Furnace_Slag', 'Fly_Ash', 'Superplasticizer', 'Age'],
                                             data=np.array([Cement, Blast_Furnace_Slag, Fly_Ash, Superplasticizer, Age]).reshape(1, 5)))
-    print(prediction)
+    # print(prediction)
     return str(np.round(prediction[0], 2))
 
 
